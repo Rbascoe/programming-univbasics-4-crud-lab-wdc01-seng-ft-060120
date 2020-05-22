a058885e591f2f806601a6de23c42d8a492eab8d
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-22 18:48:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,5 +40,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  retrieve_element_from_index[1] = "hash"
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[1] = "hash"
 end
